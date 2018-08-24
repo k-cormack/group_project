c0ccc391fa7d0a73470fccda86d4120f478d6a47
Mary Brady <emceebrady@gmail.com>
2018-08-24 15:37:31 -0600

    votes

--- server/models/post.js
+++ server/models/post.js
@@ -5,6 +5,12 @@ let Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
 let schemaName = 'Post'
 
+
+let voteSchema = new Schema({
+    userId: { type: ObjectId, ref: 'User', required: true, unique: true },
+    value: { type: Number, min: -1, max: 1, required: true }
+})
+
 let schema = new Schema({
     title: {
         type: String,
@@ -31,9 +37,7 @@ let schema = new Schema({
         type: Array
     },
 
-    voteScore: {
-        type: Number
-    }
+    votes: [voteSchema]
 })
 
 module.exports = mongoose.model(schemaName, schema)
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,34 +5,49 @@ router.get('./by-user/:userId', (req, res, next) => {
     Posts.find({
         userId: req.params.id
     })
-    .then(postList => {
-        res.send(postList)
-    })
-    .catch(next)
+        .then(postList => {
+            res.send(postList)
+        })
+        .catch(next)
 })
 
 router.post('/', (req, res, next) => {
     Posts.create(req.body)
-    .then(post => {
-        res.send(post)
-    })
-    .catch(next)
+        .then(post => {
+            res.send(post)
+        })
+        .catch(next)
 })
 
 router.put('/:id', (req, res, next) => {
     Posts.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => res.send({
-        message: 'Successfully Poated!'
-    }))
-    .catch(next)
+        .then(() => res.send({
+            message: 'Successfully Poated!'
+        }))
+        .catch(next)
 })
+//VOTE FOR A POST UP OR DOWN OR 0
+router.put('/:id/vote', (req, res, next) => {
+    Posts.findById(req.params.id)
+        .then((post) => {
+            let alreadyVoted = post.votes.find(v => v.userId == req.body.userId)
+            if (alreadyVoted) {
+                alreadyVoted.value = req.body.value
+            } else {
+                post.votes.push(req.body)
+            }
+            post.save()
+        })
+        .catch(next)
+})
+
 
 router.delete('/:id', (req, res, next) => {
     Posts.findByIdAndRemove(req.params.id)
-    .then(() => res.send({
-        message: 'Post has been Deleted'
-    }))
-    .catch(next)
+        .then(() => res.send({
+            message: 'Post has been Deleted'
+        }))
+        .catch(next)
 })
 
 module.exports = router
