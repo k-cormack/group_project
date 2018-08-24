731342ac8bc3bc6af52937b06057ae845267a14b
k-cormack <k.cormack@gmail.com>
2018-08-24 11:42:12 -0600

    user login route setup

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,29 @@
-let router = require('express').Router
+let router = require('express').Router()
 let Users = require('../models/user')
 
-//Get ALL
+//User LOGIN
+
+router.post('/login', (req, res, next) => {
+    Users.findOne({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(user => {
+        if (user) {
+            return res.send(user)
+        }
+        return res.status(401).send({
+            error: 'Invalid Login'
+        })
+    })
+})
+
+router.post('/register', (req, res, next) => {
+    Users.create(req.body).then(user => {
+        res.send(user)
+    })
+    .catch(next)
+})
+
+module.exports = router
+
