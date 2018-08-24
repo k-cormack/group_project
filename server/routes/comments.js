39c5585590691164949210490a307a18f127b946
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-24 16:32:09 -0600

    updated comment route for  votes

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -16,6 +16,30 @@ router.post('/', (req, res, next) => {
     .catch(next)
 })
 
+router.put('/:id', (req, res, next) => {
+  Comments.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.send({
+      message: 'Successfully Commented!'
+    }))
+    .catch(next)
+})
+//VOTE FOR A POST UP OR DOWN OR 0 (for Comments)
+router.put('/:id/vote', (req, res, next) => {
+  Comments.findById(req.params.id)
+    .then((comment) => {
+      let alreadyVoted = comment.votes.find(v => v.userId == req.body.userId)
+      if (alreadyVoted) {
+        alreadyVoted.value = req.body.value
+      } else {
+        comment.votes.push(req.body)
+      }
+      return comment.save()
+    })
+    .then(() => res.send({ message: "Thanks for the vote on this comment" }))
+    .catch(next)
+})
+
+
 router.delete('/:id', (req, res, next) => {
   Comments.findByIdAndRemove(req.params.id)
     .then(() => res.send({
