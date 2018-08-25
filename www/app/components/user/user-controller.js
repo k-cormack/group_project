20561671c86775a9cc7e8e07a180e12da2cada51
Don Davis <dcaa2005@hotmail.com>
2018-08-24 18:46:26 -0600

    created new post input

--- www/app/components/user/user-controller.js
+++ www/app/components/user/user-controller.js
@@ -8,7 +8,14 @@ function draw() {
   userLogin.innerHTML =
     `
   <h1>Hello, ${store.state.user.userName}</h1>
-  <button onclick="app.controllers.post.getPosts()" class="btn-btn-primary">See Posts!</button>
+  <button onclick="app.controllers.post.getPosts()" class="btn btn-primary">See Posts!</button>
+  `
+  document.getElementById("new-post").innerHTML = `
+    <form onsubmit="app.controllers.post.createPost(event)>
+      <input type="text" name="description" placeholder="Enter comments here" />
+      <input type="url" name="imgUrl" placeholder="Image URL here" />
+      <button type="submit" class="btn btn-primary">Submit Post</button>
+    </form> 
   `
 }
 
